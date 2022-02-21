9c03b1b34481b6646a11f1d4e3191c5ec399b180
Karolyne Daltro <karolyne.daltro@yahoo.com.br>
2022-02-21 14:53:49 -0300

    Segundo commit

---
+++ PrimeiraConversa.kt
@@ -0,0 +1,20 @@
+fun main() {
+    /* Conversas entre colegas sobre a Generation, Fernando e Karen. */
+
+    println("Fernando: Oi Karen, tudo bem?")
+    println("Karen: Oi, quanto tempo, estou mais ou menos.")
+    println("Fernando: Como assim, me conte.")
+    println("Karen: Estou meio parada, não sei o que fazer.")
+    println("Fernando: Hmm, você tem preferência em alguma area?")
+    println("Karen: Hmm, estava pensando em algo na tecnológia, mas não sinto confiança.")
+    println("Fernando: Por quê?")
+    println("Karen: Porque deve ser muito dificil, não tenho experiência e não sou boa em exatas.")
+    println("Fernando: Ahh, não se preocupe com isso, tenho uma indicação para você.")
+    println("Karen: Sériooo??? Me fale.")
+    println("Fernando: Tem uma empresa chamada Generation que disponibiliza cursos para a area de trabalho.")
+    println("Karen: Nossaaa!!! Que maravilha, não conhecia, aonde que me cadastro?")
+    println("Fernando: No proprio site da Generation ou no istragram, vai ter alguns testes e pronto.")
+    println("Karen: Nossa amigo, muito obrigada, não sei o que seria da minha vida sem você!!!")
+    println("Fernando: Amigos servem para isso, estou sempre aqui quando precisar.")
+    println("* Karen conseguiu passar, se formou e hoje trabalha junto com Fernando. *")
+}
